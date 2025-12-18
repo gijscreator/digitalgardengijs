cdd32dcc66eeab5d473c14876e02018b9b13f458
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-12-18 12:37:50 +0100

    menu css added for smooth in and out

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,47 @@
+// Animatie voor de auto, 2 banden en de auto links naar rechts 
+
 const car = document.querySelector('.porsche');
 const leftTyre = document.querySelector('.lefttyre');
 const rightTyre = document.querySelector('.righttyre');
 
-car.addEventListener('click', () => {
+car.addEventListener('click', drivingCar)
+
+function drivingCar () {
   car.classList.toggle('driveby');
   leftTyre.classList.toggle('tyrespin');
   rightTyre.classList.toggle('tyrespin');
-  
-});
+}
+
+
+// Menu drawer met band als icon
 
 const navtyre = document.querySelector('.navtyre')
 
-navtyre.addEventListener('click', () => {
+navtyre.addEventListener('click', navTyre)
+
+function navTyre () {
   navtyre.classList.toggle('rotateme');
-  console.log("im rotating")
-  
-});
+}
+
+navtyre.addEventListener('animationend', navTyre)
+
+// Menu drawer zelf met animaties
 
 const menudrawer = document.querySelector('.menudrawer')
 
-navtyre.addEventListener('click', () => {
-  menudrawer.classList.toggle('open')
+navtyre.addEventListener('click', menuDrawer)
 
-});
+function menuDrawer () {
 
-const tireLocation = 
+  if (menudrawer.classList.contains('open')) {
+    menudrawer.classList.remove('open');
+    menudrawer.classList.add('closed');
+  } else {
+    menudrawer.classList.add('open');
+    menudrawer.classList.remove('closed');
+  }
+}
+
+
 
 // slipsporen toevoegen
